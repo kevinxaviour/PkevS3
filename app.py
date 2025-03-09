f6e872af1ac6346f12fb3415e29c44c55726b4fd
kevinxaviour <kevinxaviour22@gmail.com>
2025-03-09 22:51:39 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from typing import List, Dict, Callable, Any
 st.set_page_config(
     page_title="Porkallam Season 3",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 # GitHub repository information
